96416e449334fbc81443bc51d48d6d7673b38d52
Thorsten Ehlers <thorsten.ehlers@googlemail.com>
2014-08-03 22:14:46 +0200

    Added script to create template for new recipe

---
+++ bin/new-recipe.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+
+# Ein Aufruf von bin/new-recipe.sh <Name des Rezeptes> legt im
+# Verzeichnis _posts eine Datei für das Rezept an und befüllt
+# swoeit möglich die Metadaten.
+#
+
+set -e
+
+if (( $# != 1 )); then
+    echo "Usage: new-recipe.sh <name of recipe>"
+    exit 1
+fi
+
+RECIPES_DIR=$(dirname $0)/../_posts/
+TITLE=$1
+NORMALIZED_TITLE=$(echo ${TITLE,,} | sed -e 's-ä-ae-g' \
+                                 -e 's-ö-oe-g' \
+                                 -e 's-ü-ue-g' \
+                                 -e 's-ß-ss-g' \
+                                 -e 's-\ -\--g')
+RECIPE_FILE="${RECIPES_DIR}$(date +%Y-%m-%d)-${NORMALIZED_TITLE}.md"
+
+cat > $RECIPE_FILE <<EOF
+---
+layout: recipe
+title: "$TITLE"
+date: $(date +"%Y-%m-%d %H:%M:%S")
+categories: 
+tags: 
+---
+
+## Zutaten
+
+## Zubereitung
+
+EOF
